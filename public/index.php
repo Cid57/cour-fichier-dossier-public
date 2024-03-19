0865e014270f1bdbe5aef8388b52d7ae8ed0c19b
Cindy Singer <cindy_singer913@msn.com>
2024-03-19 11:33:11 +0100

    premier commit

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+// recupération du parametre de la page
+$page = !empty($_GET['page']) ? $_GET['page'] : 'index';
+
+//verification de l'existence de la page
+$path = '../src/pages/' . $page . '.php';
+if (file_exists($path)) {
+
+    require $path;
+
+    require '../templates/' . $page . '.html.php';
+} else {
+    // Erreur 404
+    header('HTTP/1.1 404 Not Found');
+    require '../templates/404.html.php';
+    exit;
+}
